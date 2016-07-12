d629efcbd38ffdde87e874660e263a1adfd85cee
Denis Abarca Quesada <abarcaquesada@gmail.com>
2016-07-12 13:00:54 -0600

    Clase juego aniadida

--- Juego Poker/Juego.cpp
+++ Juego Poker/Juego.cpp
@@ -1,16 +1,41 @@
 #include "stdafx.h"
 #include "Juego.h"
+#include <list>
 
-Juego::Juego()
-{
+Juego::Juego(){
+
+	Mesa* laMesa = new Mesa();
+	
 }
 
-Juego::~Juego()
-{
+Juego::~Juego(){
 }
 
-void Juego::iniciarJuego()
-{
+void Juego::iniciarJuego(){
+
+	cout << "Bienvenido al juego de poker" << endl;
+	ingresarNumeroJugadores();
+	crearJugadores();
+
+}
+
+void Juego::ingresarNumeroJugadores(){
+
+	//Esto es necesario porque hay que validar que el numero de jugadores sean como minimo 2 y maximo 6.
+	do {
+		cout << "Para iniciar digite el numero de jugadores humanos que participaran" << endl;
+		cin >> numeroJugadores;
+	} while (numeroJugadores < 2 || numeroJugadores > 6);
+
+}
+
+void Juego::crearJugadores(){
+
+	for (int i = 0; i < numeroJugadores; i++) {
+
+		losJugadores.push_front(new Jugador());
+		
+	}
 
 }
 
--- Juego Poker/Juego.h
+++ Juego Poker/Juego.h
@@ -1,13 +1,25 @@
 #pragma once
 #include "stdafx.h"
+#include "Jugador.h"
+#include "Mesa.h"
+#include "Combinaciones.h"
 
 using namespace std;
 
 class Juego {
+
+private:
+
+	int numeroJugadores;
+	list<Jugador*> losJugadores;
+	Mesa* laMesa;
+
 
 public:
 
 	Juego();
 	~Juego();
 	void iniciarJuego();
+	void ingresarNumeroJugadores();
+	void crearJugadores();
 };
